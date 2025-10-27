89ed9368be01532b2ce0cc4bd4b5abc88f90479c
surmavagit <georgesurmava@gmail.com>
2025-10-27 17:01:00 +0100

    add better argument flags, help text

--- restore.sh
+++ restore.sh
@@ -2,12 +2,25 @@
 
 printhelp () {
 	echo "
-no argument	by default prints the list of leaf commits that don't have a branch pointing to them
--l --leaves	prints the list of all leaf commits
--h --help	prints this help message
+USAGE: branch-restore.sh [OPTION]
+Print a list of git commit hashes to standard output.
+Only those commits are considered that:
+- do not have children,
+- do not have a branch head pointing to them.
 
 branch-restore creates a 'branch-restore' directory for temporary files.
-It will not run if such a directory already exists."
+It will not run if such a directory already exists.
+
+branch-restore has to be run from the main directory of the git repository.
+Either add it to the PATH or provide a relative path to branch-restore.sh file.
+
+	-a, --all	prints the list of all commits without children
+	-h, --help	prints this help message
+
+EXAMPLES:
+	branch-restore.sh | xargs -n1 git cat-file -p
+	branch-restore.sh | fzf --preview='git cat-file -p {}' | xargs git checkout
+	branch-restore.sh -a | fzf --preview='grep -rl {} .git/refs/heads/ | xargs basename 2>/dev/null || echo no branch'"
 }
 
 if [[ $# -gt 1 ]]; then
@@ -17,7 +30,7 @@ if [[ $# -gt 1 ]]; then
 fi
 
 LEAVES=0
-if [[ "$1" = "-l" ]] || [[ "$1" = "--leaves" ]]; then
+if [[ "$1" = "-a" ]] || [[ "$1" = "--all" ]]; then
 	LEAVES=1
 elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
 	printhelp
